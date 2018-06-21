3a2298550f9ccae908da7f6cdc759ddfedcb2bd7
SantaHub <pbaustin96@gmail.com>
2018-06-21 15:45:06 +0530

    Partial

--- m2/src/sources.mk
+++ m2/src/sources.mk
@@ -10,8 +10,10 @@
 #*****************************************************************************
 
 # Add your Source files to this variable
-SOURCES =
-
+SOURCES = main.c \
+	memory.c \
+	startup_msp432p401r_gcc.c \
+	startup_msp432p401r.c 
 # Add your include paths to this variable
-INCLUDES = 
+INCLUDES = -I ../include/common
 
